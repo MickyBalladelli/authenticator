117f50e5cc33fda450695943564528b17e470a85
MickyBalladelli <micky.balladelli@gmail.com>
2026-08-23 13:32:46 +0200

    new version

--- Casks/mac-authenticator.rb
+++ Casks/mac-authenticator.rb
@@ -1,6 +1,6 @@
 cask "mac-authenticator" do
-  version "1.0.0"
-  sha256 "bde56aaf5a0984b46bb7feb95bb7b887f40ff67a6bcfe267ba02887f96a16de2"
+  version "1.1.1"
+  sha256 "38cb17fa567195a102ef3603b39fa9003280fb10c03f8c735a637039e85cb728"
 
   url "https://github.com/MickyBalladelli/authenticator/releases/download/v#{version}/MacAuthenticator-#{version}.zip"
   name "Mac Authenticator"
